8232dc8702f107c1ac22733a5eb84072332b2635
Jason Badwal <jason.badwal@ontariotechu.net>
2024-07-05 18:33:25 -0400

    first commit

---
+++ Part I - Basic Python Programming Concepts/Chapter 1 - Python Basics/basics.py
@@ -0,0 +1,15 @@
+print(2 + 2) # This is an expression as it had 2 numbers seperated by an operator
+print('\n')
+
+# There are many operators such as ...
+# Exponent (**), e.g 2**3 = 8
+# Modulus/remainder (%), e.g 22%8 = 6
+# Integer division/floored quotient (//), e.g 22//8 = 2
+# Division (/), e.g 22/8 = 2.75
+# Multiplication (*), e.g 3*5 = 15
+# Addition (+), e.g 2+2 = 4
+# Bedmass also is followed in python
+
+# The Integer, Floating-Point, and String Data Types
+# A data type is a category fo values, and every value belongs to exactly one data type
+# Some of the most common data types include integers, floating point numbers, and strings
