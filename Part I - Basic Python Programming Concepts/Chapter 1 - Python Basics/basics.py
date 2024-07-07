2106a880f36325afecc57264bef0cd60d49f5e20
Jason Badwal <jason.badwal@ontariotechu.net>
2024-07-06 23:49:43 -0400

    commit

--- Part I - Basic Python Programming Concepts/Chapter 1 - Python Basics/basics.py
+++ Part I - Basic Python Programming Concepts/Chapter 1 - Python Basics/basics.py
@@ -13,3 +13,7 @@ print('\n')
 # The Integer, Floating-Point, and String Data Types
 # A data type is a category fo values, and every value belongs to exactly one data type
 # Some of the most common data types include integers, floating point numbers, and strings
+
+# We can use "+" operator on strings to concatinate them together
+# If we use one 2 strings it will work fine, but using it on an int and string will cause an error
+# The "*" operator can be used to repeate a string a certian number of time and can only be a string concatinated with an integer
